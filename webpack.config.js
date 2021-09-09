14f65ab500f8c65ade1abc01ae60e14561a39598
unknown <you@example.com>
2021-09-09 07:59:23 +0000

    c2

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ sharedMappings.register(
 module.exports = {
   output: {
     uniqueName: "productlistMfe",
-    publicPath: "/productlistmfe/latest"
+    publicPath: "auto"
   },
   optimization: {
     runtimeChunk: false
